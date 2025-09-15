678372a71abe048c078c6254b2ed0f69ab24c278
jessicahollander05 <jessicahollander2005@gmail.com>
2025-09-15 13:15:16 -0400

    add account pages

---
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+export default function Signup() {
+    return (
+        <div id="wd-signup-screen">
+            <h3>Sign up</h3>
+            <input placeholder="username" /><br />
+            <input placeholder="password" type="password" /><br />
+            <input placeholder="verify password" type="password" /><br />
+            <Link href="Profile" > Sign up </Link><br /> // had to change the "to="" as it was an invalid prop
+            <Link href="Signin" >Sign in</Link>
+        </div>
+    );
+}
