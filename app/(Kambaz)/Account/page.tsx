29f56784933fa57fab22217519a80988c22ed03f
jessicahollander05 <jessicahollander2005@gmail.com>
2025-09-16 17:04:21 -0400

    add account screen header

--- app/(Kambaz)/Account/page.tsx
+++ app/(Kambaz)/Account/page.tsx
@@ -2,5 +2,9 @@ import { redirect }
     from "next/dist/client/components/navigation";
 
 export default function AccountPage() {
+    <div id="wd-account">
+        <h1>Account</h1>
+    </div>
     redirect("/Account/Signin");
+
 }
