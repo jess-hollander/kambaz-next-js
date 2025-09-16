5788b3d4b42f263a0fa0b3f87426a25b2d8ca413
jessicahollander05 <jessicahollander2005@gmail.com>
2025-09-16 18:00:15 -0400

    fix block comment

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,7 +3,7 @@ export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
-     /*
+     {/*
       <ul>
         <li>
           <Link href="/Labs/Lab1" id="wd-lab1-link">
@@ -14,8 +14,7 @@ export default function Labs() {
         <li>
           <Link href="/Labs/Lab3" id="wd-lab3-link">
             Lab 3: JavaScript Fundamentals </Link> </li>
-      </ul> */
-
+      </ul> */}
       redirect(/Labs/Lab1);
     </div>
   );
