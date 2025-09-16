272a8e0fa237f0ca99ec698ba7180a5fcb4147de
jessicahollander05 <jessicahollander2005@gmail.com>
2025-09-16 18:04:55 -0400

    fix labs layout

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -4,15 +4,18 @@ import TOC from "./TOC";
 export default function LabsLayout({ children }:
     Readonly<{ children: ReactNode }>) {
     return (
-        <table>
-            <tbody>
-                <tr>
-                    <td valign="top" width="100px">
-                        <TOC />
-                    </td>
-                    <td valign="top">{children}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div>
+            <h1>Labs</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <td valign="top" width="100px">
+                            <TOC />
+                        </td>
+                        <td valign="top">{children}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     );
 }
