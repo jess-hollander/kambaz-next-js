8da46e791949e49f9c88c401aca56c03e5dfc67e
jessicahollander05 <jessicahollander2005@gmail.com>
2025-10-03 17:06:00 -0400

    implement more styling

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -34,7 +34,10 @@ export default function Modules() {
                 <ListGroupItem className="wd-module
                                p-0 mb-5 fs-5 border-gray">
                     <div className="wd-title p-3 ps-2 bg-secondary">
-                        Week 2</div>
+                        <BsGripVertical className="me-2 fs-3" />
+                        Week 2
+                        <ModuleControlButtons />
+                    </div>
                     <ListGroup className="wd-lessons rounded-0">
                         <ListGroupItem className="wd-lesson p-3 ps-1">
                             LESSON 1 </ListGroupItem>
---
+++ app/(Kambaz)/index.tsx
@@ -0,0 +1,56 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard/page";
+import KambazNavigation from "./Navigation";
+import Signin from "./Account/Signin/page";
+import CourseNavigation from "./Courses/[cid]/Navigation";
+import { FaAlignJustify } from "react-icons/fa";
+import Home from "./Courses/[cid]/Home/page";
+import Modules from "./Courses/[cid]/Modules/page";
+import Assignments from "./Courses/[cid]/Assignments/page";
+import AssignmentEditor from "./Courses/[cid]/Assignments/[aid]/page";
+import CourseStatus from "./Courses/[cid]/Home/Status";
+import "./styles.css";
+export default function Kambaz() {
+    return (
+<div>
+    <div id="wd-kambaz">
+        <KambazNavigation />
+        <div  className="wd-main-content-offset p-3">
+            <Routes>
+                <Route path="/" element={<Navigate to="Account" />} />
+                <Route path="/Account/*" element={<Signin />} />
+                <Route path="/Dashboard" element={<Dashboard />} />
+                <Route path="/Courses/:cid/*" element={<Home />} />
+            </Routes>
+        </div>
+    </div>
+
+    <div id="wd-courses">
+        <h2 className="text-danger">
+            <FaAlignJustify className="me-4 fs-4 mb-1" />
+            Course 1234 </h2> <hr />
+        <div className="d-flex">
+            <div className="d-none d-md-block">
+                <CourseNavigation />
+            </div>
+            <div className="flex-fill">
+                <Routes>
+                    <Route path="Home" element={<Home />} />
+                    <Route path="Modules" element={<Modules />} />
+                    <Route path="Assignments" element={<Assignments />} />
+                    <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+                </Routes>
+            </div></div>
+    </div>
+    <div className="d-flex" id="wd-home">
+        <div className="flex-fill me-3">
+            <Modules />
+        </div>
+        <div className="d-none d-xl-block">
+            <CourseStatus />
+        </div>
+    </div>
+
+</div>
+    );
+}
