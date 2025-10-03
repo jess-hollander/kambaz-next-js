f814d4746dfed6fe50bc87682ac20295b1966c73
jessicahollander05 <jessicahollander2005@gmail.com>
2025-10-03 15:49:48 -0400

    fix module buttons

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -3,6 +3,7 @@ import { ListGroup, ListGroupItem } from "react-bootstrap";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
 
 export default function Modules() {
     return (
@@ -14,7 +15,7 @@ export default function Modules() {
                     <div className="wd-title p-3 ps-2 bg-secondary">
                         <BsGripVertical className="me-2 fs-3" />
                         Week 1
-                        <ModulesControls />
+                        <ModuleControlButtons />
                     </div>
                     <ListGroup className="wd-lessons rounded-0">
                         <ListGroupItem className="wd-lesson p-3 ps-1">
