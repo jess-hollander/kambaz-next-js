7adc192126bfb412bffb16fce2d7d0f11fe3b0ab
jessicahollander05 <jessicahollander2005@gmail.com>
2025-10-17 20:04:14 -0400

    fix ternary opersator

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -28,6 +28,7 @@ import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
 import PathParameters from "./PathParameters";
+import TernaryOperator from "./TernaryOperator";
 import TodoList from "./todos/TodoList";
 export default function Lab3() {
   console.log('Hello World!');
@@ -69,6 +70,7 @@ export default function Lab3() {
         vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
      </Highlight>
      <PathParameters/>
+     <TernaryOperator/>
      <TodoList/>
     </div>
   );
